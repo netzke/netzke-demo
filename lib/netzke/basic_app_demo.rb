c9a42152fe20c30acb9e5f45133219a024180aa6
Sergei Kozlov <s.n.kozlov@gmail.com>
2009-02-06 14:49:24 -0600

    minor update: clerks and bosses views are now BorderLayoutPanel-based

--- lib/netzke/basic_app_demo.rb
+++ lib/netzke/basic_app_demo.rb
@@ -38,23 +38,67 @@ module Netzke
     def initial_late_aggregatees
       {
         :clerks => {
-          :widget_class_name => "GridPanel", 
-          :data_class_name => "Clerk", 
+          :widget_class_name => "BorderLayoutPanel",
           :ext_config => {
-            :title => "Clerks",
-            :rows_per_page => 20,
-            :config_tool => true
+            :title => false
+          },
+          :regions => {
+            :center => {
+              :widget_class_name => "GridPanel", 
+              :data_class_name => "Clerk", 
+              :ext_config => {
+                :title => 'Clerks',
+                :rows_per_page => 20,
+                :config_tool => true
+              }
+            },
+            :south => {
+              :widget_class_name => "Panel",
+              :region_config => {
+                :height => 150,
+                :split => true
+              },
+              :ext_config => {
+                :title => false,
+                :body_style => "padding: 5px",
+                :html => %Q{
+What you see is a BorderLayoutPanel-based compound widget, containing a GridPanel interfacing the Clerks data, and a Panel with a little explanation (the one you are reading). <br>What is here to play with: <br>1) Do some on-the-fly configuration of the grid - move around or resize its columns, change something in the columns configuration panel (click the tool-button in the up-right corner), and then log out and again in - you'll see that your changes got stored; <br>2) Change the size of this region - it'll get stored for you as well, by to the BorderLayoutPanel widget. <br>The same explanation naturally applies to the <a href="#bosses">bosses</a> view.
+}
+              }
+            }
           }
         },
+
         :bosses => {
-          :widget_class_name => "GridPanel", 
-          :data_class_name => "Boss", 
+          :widget_class_name => "BorderLayoutPanel",
           :ext_config => {
-            :title => "Bosses",
-            :rows_per_page => 20,
-            :config_tool => true
+            :title => false
+          },
+          :regions => {
+            :center => {
+              :widget_class_name => "GridPanel", 
+              :data_class_name => "Boss", 
+              :ext_config => {
+                :title => "Bosses",
+                :rows_per_page => 20,
+                :config_tool => true
+              }
+            },
+            :south => {
+              :widget_class_name => "Panel",
+              :region_config => {
+                :height => 50,
+                :split => true
+              },
+              :ext_config => {
+                :title => false,
+                :body_style => "padding: 5px",
+                :html => %Q{See the explanations for the <a href="#clerks">clerks</a> view.}
+              }
+            }
           }
         },
+
         :users => {
           :widget_class_name => "GridPanel", 
           :data_class_name => "User", 
