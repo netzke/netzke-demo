97e4defe39c9f8a9a05006ed0cbd31506ff7b688
Sergei Kozlov <s.n.kozlov@gmail.com>
2009-02-12 19:02:16 +0100

    disabled delete button in user_manager

--- lib/netzke/user_manager.rb
+++ lib/netzke/user_manager.rb
@@ -5,20 +5,30 @@ module Netzke
     end
     def initialize(*args)
       super
-      config[:grid_config]    = {:columns => [:id, :login, :login_count, :last_login_at, :last_request_at]}
-      config[:form_config]    = {:fields => [
-        :id, 
-        :login, 
-        {:name => :login_count, :disabled => true}, 
-        {:name => :password, :inputType => 'password'}, 
-        {:name => :password_confirmation, :inputType => 'password'}, 
-        {:name => :last_request_at, :disabled => true}, 
-        {:name => :current_login_at, :disabled => true}, 
-        {:name => :last_login_ip, :disabled => true}, 
-        {:name => :current_login_ip, :disabled => true}, 
-        {:name => :created_at, :disabled => true},
-        {:name => :updated_at, :disabled => true}
-      ]}
+      config[:grid_config]    = {
+        :columns => [
+          :id, 
+          :login, 
+          :login_count, 
+          :last_login_at, 
+          :last_request_at],
+        :prohibit => :delete
+      }
+      config[:form_config]    = {
+        :fields => [
+          :id, 
+          :login, 
+          {:name => :login_count, :disabled => true}, 
+          {:name => :password, :inputType => 'password'}, 
+          {:name => :password_confirmation, :inputType => 'password'}, 
+          {:name => :last_request_at, :disabled => true}, 
+          {:name => :current_login_at, :disabled => true}, 
+          {:name => :last_login_ip, :disabled => true}, 
+          {:name => :current_login_ip, :disabled => true}, 
+          {:name => :created_at, :disabled => true},
+          {:name => :updated_at, :disabled => true}
+        ]
+      }
 
       config[:split_region]    = :east
       config[:split_size]      = 400
