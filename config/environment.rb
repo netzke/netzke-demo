5da9a61107836b753e85dcbb4e761c0e4b1713e4
Sergei Kozlov <s.n.kozlov@gmail.com>
2009-09-06 20:09:07 -0500

    netzke required from github

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -28,7 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-  config.gem "netzke-basepack"
+  config.gem "skozlov-netzke-basepack", :lib => "netzke-basepack"
   config.gem "faker"
   config.gem "authlogic"
   
