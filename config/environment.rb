c597f76eba34d8b10227f0c4dc6aef4c0956788f
Sergei Kozlov <skozlov@Sergei-Kozlovs-MacBook-Pro.local>
2009-09-24 10:00:31 -0500

    now dependent on netzke-basepack (from rubyforge)

--- config/environment.rb
+++ config/environment.rb
@@ -28,7 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   
-  config.gem "skozlov-netzke-basepack", :lib => "netzke-basepack"
+  config.gem "netzke-basepack"
   config.gem "faker"
   config.gem "authlogic"
   
