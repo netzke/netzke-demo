e421daa3cf57ff61a495a06561b357162e52e0e2
skozlov <sergei@writelesscode.com>
2010-06-10 00:24:50 +0200

    nicer link for BasicApp demo

--- app/controllers/basic_app_controller.rb
+++ app/controllers/basic_app_controller.rb
@@ -1,12 +1,12 @@
 class BasicAppController < ApplicationController
-  # before_filter :require_user, :only => :demo
 
   def intro
     render :layout => false
   end
-  
-  def index
-    redirect_to :action => "intro"
+
+  # to not break someone's links
+  def demo
+    redirect_to :action => "index", :status=>:moved_permanently
   end
 
 end
