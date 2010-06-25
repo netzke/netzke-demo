0b85fdba14e047bd622d5abf2bc29de7905ff6fa
skozlov <sergei@writelesscode.com>
2010-06-25 12:13:40 +0200

    the associations example of GridPanel is extended with specifying a scope for association records to be chosen from in the combobox

--- lib/netzke/model_extensions/clerk_for_grid_panel.rb
+++ lib/netzke/model_extensions/clerk_for_grid_panel.rb
@@ -10,6 +10,9 @@ module Netzke::ModelExtensions
     # Preconfigure a "real" attribute
     netzke_attribute :salary, :renderer => "usMoney"
     
+    # Set the scopes for bosses listed in the association column
+    netzke_attribute :boss__last_name, :editor => {:xtype => :combobox, :scopes => [["salary_greater_than", 95000]]}
+    
     # Specify which columns and in which order to show
     netzke_expose_attributes :id, :name, :first_name, :last_name, :updated_bulb, :email, :salary, :boss__last_name      
     
