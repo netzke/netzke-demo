e94127c36f4c400147d44b9843664b72a8394c0f
Sergei Kozlov <sergei@playcode.nl>
2010-10-08 14:27:55 +0200

    Switched to the AR session store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100925153708) do
+ActiveRecord::Schema.define(:version => 20101008122627) do
 
   create_table "bosses", :force => true do |t|
     t.string   "first_name"
@@ -40,5 +40,15 @@ ActiveRecord::Schema.define(:version => 20100925153708) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
