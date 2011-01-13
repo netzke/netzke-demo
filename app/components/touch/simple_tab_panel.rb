53717679b9d2c3607fd39407b9f52670456e2ca9
Sergei Kozlov <sergei@playcode.nl>
2011-01-13 10:39:14 +0100

    Comments and readability of Touch examples

--- app/components/touch/simple_tab_panel.rb
+++ app/components/touch/simple_tab_panel.rb
@@ -2,6 +2,7 @@ module Touch
   class SimpleTabPanel < Netzke::Base
     js_base_class "Ext.TabPanel"
 
+    # Some extra configuration for the TabPanel
     js_properties(
       :fullscreen => true,
       :docked_items => [{:dock => :top, :xtype => :toolbar, :title => 'Netzke Demo: SimpleTabPanel'}]
@@ -11,7 +12,18 @@ module Touch
       super.merge({:items => [:bosses.component, :clerks.component]})
     end
 
-    component :bosses, :class_name => "Touch::SimpleList", :title => "Bosses", :model => "Boss", :item_tpl => "{last_name}, ${salary}"
-    component :clerks, :class_name => "Touch::SimpleList", :title => "Clerks", :model => "Clerk", :item_tpl => "{last_name}"
+    component :bosses, {
+      :class_name => "Touch::SimpleList",
+      :title => "Bosses",
+      :model => "Boss",
+      :item_tpl => "{last_name}, ${salary}"
+    }
+
+    component :clerks, {
+      :class_name => "Touch::SimpleList",
+      :title => "Clerks",
+      :model => "Clerk",
+      :item_tpl => "{last_name}"
+    }
   end
 end
