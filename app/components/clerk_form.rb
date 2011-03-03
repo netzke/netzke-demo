fd83652208ee5e510f6092bf91d5734aa0c116b2
Sergei Kozlov <sergei@playcode.nl>
2011-03-03 16:18:45 +0100

    Minor fix

--- app/components/clerk_form.rb
+++ app/components/clerk_form.rb
@@ -6,7 +6,7 @@ class ClerkForm < Netzke::Basepack::PagingFormPanel
 
   def configuration
     boss_basic_info = [
-      {:field_label => "First name", :name => :boss__name, :read_only => true},
+      {:field_label => "First name", :name => :boss__first_name, :read_only => true},
       {:field_label => "Last name", :name => :boss__last_name, :read_only => true},
       {:field_label => "Email", :name => :boss__email, :read_only => true}
     ]
@@ -22,7 +22,7 @@ class ClerkForm < Netzke::Basepack::PagingFormPanel
           {:flex => 1, :layout => :form, :border => false, :defaults => {:anchor => "-8"}, :items => [:first_name, :email]},
           {:flex => 1, :layout => :form, :border => false, :defaults => {:anchor => "100%"}, :items => [:last_name, :salary]}
         ]},
-        {:name => :boss__first_name},
+        {:name => :boss__name},
         {:xtype => :fieldset, :title => "Boss info", :items => [
           {:xtype => :tabpanel, :padding => 5, :plain => true, :active_tab => 0, :items => [{:title => "Basic info", :layout => :form, :label_align => :left, :items => boss_basic_info}, {:title => "Details", :layout => :form, :items => boss_extra_info}]}
         ]}
