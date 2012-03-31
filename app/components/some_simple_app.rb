fee495973483462a8f240751f18403473bb20c1c
Sergei Kozlov <sergei@playcode.nl>
2012-03-31 14:15:32 +0700

    Update config syntax

--- app/components/some_simple_app.rb
+++ app/components/some_simple_app.rb
@@ -2,125 +2,123 @@ class SomeSimpleApp < Netzke::Basepack::SimpleApp
 
   # Initial layout of the app.
   # <tt>status_bar_config</tt>, <tt>menu_bar_config</tt>, and <tt>main_panel_config</tt> are defined in SimpleApp.
-  def configuration
-    sup = super
-    sup.merge(
-      :items => [{
-        :region => :north,
-        :border => false,
-        :height => 35,
-        :html => %Q{
-          <div style="margin:10px; color:#333; text-align:center; font-family: Helvetica; font-size: 150%;">
-            <a style="color:#B32D15; text-decoration: none" href="http://netzke.org">Netzke</a> Demo
-          </div>
-        },
-        :bodyStyle => {:background => %Q(#FFF url("/images/header-deco.gif") top left repeat-x)}
-      },{
+  def configure
+    super
+    config.items = [{
+      :region => :north,
+      :border => false,
+      :height => 35,
+      :html => %Q{
+        <div style="margin:10px; color:#333; text-align:center; font-family: Helvetica; font-size: 150%;">
+          <a style="color:#B32D15; text-decoration: none" href="http://netzke.org">Netzke</a> Demo
+        </div>
+      },
+      :bodyStyle => {:background => %Q(#FFF url("/images/header-deco.gif") top left repeat-x)}
+    },{
+      :region => :center,
+      :layout => :border,
+      :border => false,
+      :items => [status_bar_config, {
         :region => :center,
         :layout => :border,
-        :border => false,
-        :items => [status_bar_config, {
-          :region => :center,
-          :layout => :border,
-          :items => [menu_bar_config, main_panel_config, {
+        :items => [menu_bar_config, main_panel_config, {
 
-            # Navigation
-            :region => :west,
-            :width => 250,
-            :split => true,
-            :xtype => :treepanel,
-            :title => "Navigation",
-            :root_visible => false,
-            :item_id => :navigation,
-            :root => {
-              :text => "Navigation",
+          # Navigation
+          :region => :west,
+          :width => 250,
+          :split => true,
+          :xtype => :treepanel,
+          :title => "Navigation",
+          :root_visible => false,
+          :item_id => :navigation,
+          :root => {
+            :text => "Navigation",
+            :expanded => true,
+            :children => [{
+              :text => "Simple Components",
               :expanded => true,
               :children => [{
-                :text => "Simple Components",
+                :text => "GridPanel",
                 :expanded => true,
                 :children => [{
-                  :text => "GridPanel",
-                  :expanded => true,
-                  :children => [{
-                    :text => "Bosses",
-                    :icon => uri_to_icon(:user_suit),
-                    :leaf => true,
-                    :component => "bosses"
-                  },{
-                    :text => "Clerks",
-                    :icon => uri_to_icon(:user),
-                    :leaf => true,
-                    :component => "clerks"
-                  },{
-                    :text => "Customized Clerks",
-                    :icon => uri_to_icon(:user),
-                    :leaf => true,
-                    :component => "customized_clerks"
-                  },{
-                    :text => "Custom Action Grid",
-                    :leaf => true,
-                    :component => "custom_action_grid"
-                  }]
-                },{
-                  :text => "FormPanel",
-                  :expanded => true,
-                  :children => [{
-                    :text => "Clerks Paging Form",
-                    :icon => uri_to_icon(:user),
-                    :leaf => true,
-                    :component => "clerk_paging_form"
-                  },{
-                    :text => "Clerks Paging Lockable Form",
-                    :icon => uri_to_icon(:user),
-                    :leaf => true,
-                    :component => "clerk_paging_lockable_form"
-                  },{
-                    :text => "Clerks Paging Form, custom layout",
-                    :icon => uri_to_icon(:user),
-                    :leaf => true,
-                    :component => "clerk_form_custom_layout"
-                  }]
-                }]
-              },{
-                :text => "Composite Components",
-                :expanded => true,
-                :children => [{
-                  :text => "Bosses And Clerks",
-                  :icon => uri_to_icon(:user_user_suit),
+                  :text => "Bosses",
+                  :icon => uri_to_icon(:user_suit),
                   :leaf => true,
-                  :component => "bosses_and_clerks",
+                  :component => "bosses"
                 },{
-                  :text => "Simple Clerk Inspector",
+                  :text => "Clerks",
                   :icon => uri_to_icon(:user),
                   :leaf => true,
-                  :component => "clerk_inspector"
+                  :component => "clerks"
                 },{
-                  :text => "Simple Portal",
+                  :text => "Customized Clerks",
+                  :icon => uri_to_icon(:user),
                   :leaf => true,
-                  :component => "simple_portal"
+                  :component => "customized_clerks"
+                },{
+                  :text => "Custom Action Grid",
+                  :leaf => true,
+                  :component => "custom_action_grid"
                 }]
               },{
-                :text => "Stand-alone Components",
-                :leaf => true,
-                :component => "embedded"
+                :text => "FormPanel",
+                :expanded => true,
+                :children => [{
+                  :text => "Clerks Paging Form",
+                  :icon => uri_to_icon(:user),
+                  :leaf => true,
+                  :component => "clerk_paging_form"
+                },{
+                  :text => "Clerks Paging Lockable Form",
+                  :icon => uri_to_icon(:user),
+                  :leaf => true,
+                  :component => "clerk_paging_lockable_form"
+                },{
+                  :text => "Clerks Paging Form, custom layout",
+                  :icon => uri_to_icon(:user),
+                  :leaf => true,
+                  :component => "clerk_form_custom_layout"
+                }]
               }]
-            }
-          }]
+            },{
+              :text => "Composite Components",
+              :expanded => true,
+              :children => [{
+                :text => "Bosses And Clerks",
+                :icon => uri_to_icon(:user_user_suit),
+                :leaf => true,
+                :component => "bosses_and_clerks",
+              },{
+                :text => "Simple Clerk Inspector",
+                :icon => uri_to_icon(:user),
+                :leaf => true,
+                :component => "clerk_inspector"
+              },{
+                :text => "Simple Portal",
+                :leaf => true,
+                :component => "simple_portal"
+              }]
+            },{
+              :text => "Stand-alone Components",
+              :leaf => true,
+              :component => "embedded"
+            }]
+          }
         }]
       }]
-    )
+    }]
   end
 
   # Components
   component :bosses,
-    :class_name => "Basepack::GridPanel",
+    :class_name => "Netzke::Basepack::GridPanel",
     :model => "Boss",
     :lazy_loading => true,
     :title => "Bosses",
     :persistence => true
 
   component :clerks,
-    :class_name => "Basepack::GridPanel",
+    :class_name => "Netzke::Basepack::GridPanel",
     :model => "Clerk",
     :lazy_loading => true,
     :title => "Clerks",
@@ -161,7 +159,7 @@ class SomeSimpleApp < Netzke::Basepack::SimpleApp
 
   # A simple panel thit will render a page with links to different Rails views that have embedded widgets in them
   component :embedded,
-    :class_name => "Basepack::Panel",
+    :class_name => "Netzke::Basepack::Panel",
     :auto_load => "demo/embedded",
     :body_padding => 15,
     :title => "Components embedded into Rails views",
