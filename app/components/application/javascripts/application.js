c42a9c8f7dc1eeb35eed1892b90c6ac8b83f9bd5
nomadcoder <nmcoder@gmail.com>
2012-10-21 18:40:46 +0200

    Minor About text update

--- app/components/application/javascripts/application.js
+++ app/components/application/javascripts/application.js
@@ -28,9 +28,9 @@
       title: "Netzke Demo",
       buttons: Ext.Msg.OK,
       icon: Ext.MessageBox.INFO,
-      msg: 'Explore demo <a target="_blank" href="http://netzke.org">Netzke</a> components along with their source code. <br/> \
-        Follow <a target="_blank" href="http://twitter.com/netzke">@netzke</a> on Twitter for the latest news on the framework. <br/> \
-        The source code for this app is on <a target="_blank" href="https://github.com/nomadcoder/netzke-demo">GitHub</a>. <br/> \
+      msg: '<p>Explore demo <a target="_blank" href="http://netzke.org">Netzke</a> components along with their source code. <p/> \
+        <p>Follow <a target="_blank" href="http://twitter.com/netzke">@netzke</a> on Twitter for the latest news on the framework. <p/> \
+        <p>The source code for this app is on <a target="_blank" href="https://github.com/nomadcoder/netzke-demo">GitHub</a>. <p/> \
         <br/> \
         <div style="text-align: right">By <a target="_blank" href="http://twitter.com/nomadcoder">nomadcoder</a>, ' + (new Date()).getFullYear() + '</div> \
         '
