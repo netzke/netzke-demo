f73ed7fa8771472e61d42905c9512ddf6604fb56
Max Gorin <max@goodbitlabs.com>
2015-12-01 14:31:53 +0700

    Increase amount of seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,44 @@
-number_of_bosses = 50
+number_of_bosses = 500
 
+puts "Generating bosses..."
 Boss.delete_all
 bosses_ids = []
-number_of_bosses.times do
+data = number_of_bosses.times.with_index.map do |i|
   first_name = Faker::Name.first_name
   email = "#{first_name.downcase}@#{Faker::Internet.email.split("@").last}"
-  a_boss = Boss.create({
+  {
+    id: i + 1,
     :first_name => first_name,
     :last_name => Faker::Name.last_name,
     :email => email,
     :salary => (rand(10)+1)*10000
-  })
-  bosses_ids << a_boss.id
+  }
 end
+Boss.create(data)
 
-
+puts "Generating clerks..."
 Clerk.delete_all
-200.times do
+timestamp = 15.minutes.ago
+data = 20000.times.map do
   first_name = Faker::Name.first_name
   email = "#{first_name.downcase}@#{Faker::Internet.email.split("@").last}"
-  Clerk.create({
-    :boss_id => bosses_ids[rand(number_of_bosses)],
+  {
+    :boss_id => rand(number_of_bosses) + 1,
     :first_name => first_name,
     :last_name => Faker::Name.last_name,
     :email => email,
     :salary => (rand(10)+1)*1000,
     :subject_to_lay_off => rand > 0.8,
-    :created_at => 15.minutes.ago,
-    :updated_at => 15.minutes.ago
-  })
+    :created_at => timestamp,
+    :updated_at => timestamp
+  }
 end
+Clerk.create(data)
 
+puts "Generating files..."
 FileRecord.delete_all
 ids = []
-200.times do |t|
+2000.times do |t|
   file = FileRecord.create({
     name: "file-#{t}",
     size: rand(100000),
