b88ae85f794b96c25a7660f14385ab3a4adb5e61
Max Gorin <max@goodbitlabs.com>
2016-05-01 08:34:39 +0300

    pg_trgm got added

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20151226061600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "bosses", force: :cascade do |t|
     t.string   "name"
