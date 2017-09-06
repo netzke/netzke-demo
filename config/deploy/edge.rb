22c9d157824ee4852de672e0745abc6725389468
Max Gorin <gorinme@gmail.com>
2017-09-06 19:57:59 +0300

    Fix deploy script

--- config/deploy/edge.rb
+++ config/deploy/edge.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{netzke-edgedemo@netzke.com}
-role :web, %w{netzke-edgedemo@netzke.com}
-role :db,  %w{netzke-edgedemo@netzke.com}
+role :app, %w{netzke-edgedemo@netzke.org}
+role :web, %w{netzke-edgedemo@netzke.org}
+role :db,  %w{netzke-edgedemo@netzke.org}
 
 
 # Extended Server Syntax
